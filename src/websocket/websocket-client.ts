e4e2840e0a56b59813cbe9a4a8cf62b90f44395d
ian <dldks1290@gmail.com>
2026-07-07 19:49:33 +0900

    더 빠르게

--- src/websocket/websocket-client.ts
+++ src/websocket/websocket-client.ts
@@ -3,7 +3,8 @@ import { Client } from "@stomp/stompjs";
 export const stompClient = new Client({
   brokerURL: import.meta.env.VITE_SOCKET_URL,
 
-  reconnectDelay: 5000,
+  reconnectDelay: 500,
+  connectionTimeout: 10000,
 
   // debug: function (str) {
   //   console.log("[STOMP Debug]: ", str);
